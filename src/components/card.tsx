1fb89235ab9cef9d529857f884cad84c7168dac4
Luis Eduardo Contreras Mendez <luise.conteras@outlook.com>
2025-12-26 21:40:29 -0500

    agregar imagenes a cards y alimentar datos de libros

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,8 @@
+
 import { ICard } from "@/interfaces/components/ICards";
 import Button from "./button";
 import useCart from "@/hooks/useCart";
 import { MouseEvent } from "react";
-import { IBooks } from "@/interfaces/lib/myBackendInterface";
 
 export default function Card({ book, onClick }: ICard) {
   const { addBook } = useCart();
@@ -12,15 +12,30 @@ export default function Card({ book, onClick }: ICard) {
     addBook(book, 1);
   };
 
+  const hasImage = Boolean(book.img);
+
   return (
     <div className="card" onClick={onClick}>
+      {/* Imagen de portada */}
+      <div className="card_image">
+
+          <img
+            src={book.image}
+            alt={`${book.title} - portada`}
+            loading="lazy"
+          />
+
+      </div>
+
       <div className="card_header">
         <h2>{book.title}</h2>
         <div className="isbn">ISBN: {book.isbn}</div>
       </div>
+
       <div className="card_body">
         <p>{book.description}</p>
       </div>
+
       <div className="card_footer">
         <p>Price: ${book.price}</p>
         <Button
@@ -33,3 +48,4 @@ export default function Card({ book, onClick }: ICard) {
     </div>
   );
 }
+
